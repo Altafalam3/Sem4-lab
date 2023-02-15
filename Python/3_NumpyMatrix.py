93dbe4e8562d2f7490624bda5c407c5691b2eb35
Altaf Alam <102013452+Altafalam3@users.noreply.github.com>
2023-02-15 22:27:45 +0530

    Create 3_NumpyMatrix.py

---
+++ Python/3_NumpyMatrix.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# create an 8x3 array of numbers from 10 to 34
+arr = np.arange(10, 35).reshape(8, 3)
+
+# split the array into 4 equal parts
+part1, part2, part3, part4 = np.split(arr, 4)
+
+print("Part 1:")
+print(part1)
+print("\nPart 2:")
+print(part2)
+print("\nPart 3:")
+print(part3)
+print("\nPart 4:")
+print(part4)
