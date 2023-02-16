b4c6d97bf256710703ce6a0c37c94475de05fa6e
Altaf Alam <102013452+Altafalam3@users.noreply.github.com>
2023-02-16 23:30:12 +0530

    Update 5_Classes.py

--- Python/5_Classes.py
+++ Python/5_Classes.py
@@ -1,2 +1,41 @@
-class vehicle:
-    def __init__(self,milage,speed):
+class Vehicle:
+    def __init__(self, mileage, max_speed, name):
+        self.mileage = mileage
+        self.max_speed = max_speed
+        self.name = name
+
+    def seating_capacity(self):
+        pass
+
+    def fare(self):
+        return (self.seating_capacity() * 20)+ (self.seating_capacity() * 20 *0.1)
+
+    def vehicle_info(self):
+        print(f"Vehicle Name: {self.name}")
+        print(f"Mileage: {self.mileage}")
+        print(f"Max Speed: {self.max_speed}")
+        print(f"Seating Capacity: {self.seating_capacity()}")
+        print(f"Fare: {self.fare()}")
+
+
+class Bus(Vehicle):
+    def __init__(self, mileage, max_speed, name):
+        super().__init__(mileage, max_speed, name)
+
+    def seating_capacity(self):
+        return 40
+
+
+class Car(Vehicle):
+    def __init__(self, mileage, max_speed, name):
+        super().__init__(mileage, max_speed, name)
+
+    def seating_capacity(self):
+        return 3
+
+
+bus = Bus(10, 100, "Volvo")
+car = Car(20, 120, "Toyota")
+
+bus.vehicle_info()
+car.vehicle_info()
