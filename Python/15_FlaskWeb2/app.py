6ae3f5cf83f25bfb133e2899f0bb3801d350b951
Altafalam3 <altaf.alam0032@gmail.com>
2023-04-15 17:21:51 +0530

    trying to organize a bit

--- Python/15_FlaskWeb2/app.py
+++ Python/15_FlaskWeb2/app.py
@@ -1,11 +1,3 @@
-'''
-Create a web app using Flask to display the result of a user. If
-marks are less than 50, redirect user to the ‘failed’ web page and
-display the appropriate result. Otherwise, redirect user to ‘passed’
-web page and display the appropriate result on that page. (Use
-redirect, jsonify and other methods as needed).
-'''
-
 from flask import Flask, redirect, jsonify
 
 app = Flask(__name__)
