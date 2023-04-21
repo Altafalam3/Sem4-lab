da802c8eef6007bb8e187971895e6ce1656f55bb
Altafalam3 <altaf.alam0032@gmail.com>
2023-04-21 17:38:49 +0530

    yayy perl working

---
+++ Unix/Shell,Perl_Scripts/KeyValue.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+%data = (1 => "altaf", 2 => "aman", 3 => "prasad", 4 => "sarah", 5 => "pratham", 6 => "mitesh");
+
+print "Enter roll no:";
+$c = <>;
+
+# for handling newlines
+chomp($c);
+
+print "\$data{$c} = $data{$c}\n";
