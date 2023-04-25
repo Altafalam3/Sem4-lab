027b920a9789ea9b43c214a8783ebba9c6b6147e
shubhamgyd <108620698+shubhamgyd@users.noreply.github.com>
2023-04-25 21:55:43 +0530

    Update 4_Function1.py

--- Python/4_Function1.py
+++ Python/4_Function1.py
@@ -8,7 +8,7 @@ def add_iterables(*args):
     """
     result = [sum(x) for x in zip(*args)]
     return result
-
+'''
 # Example 1
 print(add_iterables([1, 2, 3], [4, 5, 6]))
 # Output: [5, 7, 9]
@@ -16,3 +16,25 @@ print(add_iterables([1, 2, 3], [4, 5, 6]))
 # Example 2
 print(add_iterables([1, 2, 3], [4, 5, 6], [7, 8, 9]))
 # Output: [12, 15, 18]
+
+
+def add_iterables(*args):
+    sum_items = 0
+    a = len(args[0])
+    # print(a)
+    b = [0,0,0]
+    for j in range(a):
+        b[j] = 0
+        for item in args:
+        # print(item)
+            b[j] += item[j]
+    print(b)
+    
+print(add_iterables([1,3,3],[4,6,6]))
+
+'''
+output:
+[5,9,9]
+'''
+
+'''
