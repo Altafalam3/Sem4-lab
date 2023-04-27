77664fce70d310bda04aa0eaaab55afdd27b6b65
Altafalam3 <altaf.alam0032@gmail.com>
2023-04-27 17:38:11 +0530

    fixxxx

--- Unix/awk,grep,sed/GrepFileCount.sh
+++ Unix/awk,grep,sed/GrepFileCount.sh
@@ -1,10 +1,17 @@
-#using grep 
+#!/bin/bash
 
-echo -n "No of lines: "
-echo `grep -c "" $filepath`
+echo "Enter the filename: "
+read filename
 
-echo -n "No of words: "
-echo `grep -o '\w*' $filepath | wc -w`
+# Count number of characters
+char_count=$(grep -o '.' $filename | wc -l)
 
-echo -n "No of Characters: "
-echo `grep -o . $filepath | wc -l`
+# Count number of words
+word_count=$(grep -o '\w\+' $filename | wc -l)
+
+# Count number of lines
+line_count=$(grep -c '^' $filename)
+
+echo "Number of characters: $char_count"
+echo "Number of words: $word_count"
+echo "Number of lines: $line_count"
